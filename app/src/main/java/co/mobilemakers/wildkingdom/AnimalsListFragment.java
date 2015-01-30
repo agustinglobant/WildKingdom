29e71be69b80b4d8479dc5a47517064fddc31437
Agustín <agustin.gugliotta@globant.com>
2015-01-30 18:13:30 -0300

    Add Animal specific fragment and logic

--- app/src/main/java/co/mobilemakers/wildkingdom/AnimalsListFragment.java
+++ app/src/main/java/co/mobilemakers/wildkingdom/AnimalsListFragment.java
@@ -11,6 +11,10 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 
+import static co.mobilemakers.wildkingdom.AnimalFragment.DESCRIPTION;
+import static co.mobilemakers.wildkingdom.AnimalFragment.HABITAT;
+import static co.mobilemakers.wildkingdom.AnimalFragment.IMAGE_ID;
+import static co.mobilemakers.wildkingdom.AnimalFragment.NAME;
 /**
  * A simple {@link Fragment} subclass.
  */
@@ -18,6 +22,20 @@ public class AnimalsListFragment extends ListFragment {
 
     private String[] animals = {"Brown Bear", "Grizzly Bear", "Wombat", "Gecko", "Coral Snake",
                                 "Tortoise"};
+    private String[] description = {"Adult bears generally weigh between 100 and 635 kg (220 and 1,400 lb). Its largest subspecies, the Kodiak bear, rivals the polar bear as the largest member of the bear family and as the largest land-based predator.",
+    "Most adult female grizzlies weigh 130–200 kg (290–440 lb), while adult males weigh on average 180–360 kg (400–790 lb). Average total length in this subspecies is 198 cm (6.50 ft), with an average shoulder height of 102 cm (3.35 ft) and hindfoot length of 28 cm (11 in).",
+    "Wombats are short-legged, muscular quadrupedal marsupials that are native to Australia and are approximately 1 metre (40 in) in length, with short, stubby tails. All are members of the family Vombatidae.",
+    "Geckos are lizards belonging to the infraorder Gekkota, found in warm climates throughout the world. They range from 1.6 to 60 cm. Most geckos cannot blink, but they often lick their eyes to keep them clean and moist. They have a fixed lens within each iris that enlarges in darkness to let in more light.",
+    "Coral snakes are a large group of elapid snakes that can be subdivided into two distinct groups, Old World coral snakes and New World coral snakes.",
+    "Tortoises are a family, Testudinidae, of land-dwelling turtles in the order Testudines. Contrary to popular belief, tortoises are in fact turtles rather than part of a separate group. Like most turtles, tortoises are shielded from predators by a shell."};
+    private String[] habitat = {"Northern Eurasia and North America",
+                                "North America",
+                                "Australia",
+                                "????",
+                                "Asia",
+                                "????"};
+    private int[] image_id = {R.drawable.brown_bear, R.drawable.grizzly_bear, R.drawable.wombat,
+            R.drawable.gecko, R.drawable.coral_snake, R.drawable.tortoise};
 
     public AnimalsListFragment() {
         // Required empty public constructor
@@ -42,6 +60,21 @@ public class AnimalsListFragment extends ListFragment {
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
-        
+        AnimalFragment animalFragment = new AnimalFragment();
+        animalFragment.setArguments(createAnimalBundle(position));
+        getFragmentManager().
+                beginTransaction().
+                replace(R.id.fragment_container, animalFragment).
+                addToBackStack(null).
+                commit();
+    }
+
+    private Bundle createAnimalBundle(int position) {
+        Bundle bundle = new Bundle();
+        bundle.putString(NAME, animals[position]);
+        bundle.putString(DESCRIPTION, description[position]);
+        bundle.putString(HABITAT,habitat[position]);
+        bundle.putInt(IMAGE_ID, image_id[position]);
+        return bundle;
     }
 }
